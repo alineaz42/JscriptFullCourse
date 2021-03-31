0c714b21d4e3cbad2e135e3a8016a7c5c0ffb7fa
Ali Neaz <neazali77@gmail.com>
2021-03-31 21:13:22 +0600

    jscrit full course till array codewithHarry

---
+++ app.js
@@ -0,0 +1,128 @@
+// console.log("Hello world");
+// alert("Stop! you are not using update version of this browser.");
+
+// let b = "smoothie";
+// console.log(b);
+// var a = 45;
+// console.log(a);
+
+// let age = prompt("Hey, What is your age?");
+// document.getElementById('some-text').innerHTML = age;
+
+
+// //increment one by one
+// let number = 10;
+// number = number + 1;
+// number++;
+// consolelog(number);
+
+// //decrement one by one
+// let numb2 = 42;
+// numb2--;
+// console.log(numb2);
+
+
+// //remainder 
+// let num3 = 13;
+// let numb4 = 3;
+// numb5 = num3 % numb4;
+// console.log(numb5);  //will give us the remainder in the console
+
+
+//creating function 
+//step1: creat a function 
+//step2: call the function out side of the function
+
+
+// function greetin() {  //createdte the function
+//     console.log("This is a function");
+// }
+// greetin();  //called the function
+
+
+// function greeting() {
+//     let name = prompt("What is your name?");
+//     let result = "Hellop" + name;  //string concateing
+//     console.log(result);  //created the function 
+// }
+// greeting();  //the function is called
+
+
+//adding numbers in function simple way possible
+// function sumNumber(a, b) {
+//     let sum = a + b;
+//     console.log(sum);
+// }
+// sumNumber(4, 5); //will print 4+5=9
+// similarly we can print all the operations in the functin 
+
+//loop  
+//while loop
+// let num = 0;
+// while (num < 100) {
+//     console.log(num);
+//     num++;
+// } //will print till 0-99 
+//loops works until the condition is not fulfiled 
+
+
+//data types
+//there are many types of data 
+//such as string number null boolian undefined etc
+
+
+// let yourAge = 18;  //number
+// let yourName = "Ali Neaz"; //which is a string
+//data types can be checked by typeOf
+//arrays 
+// let myArray = ['neaz', 'apple', 34, myname = { first: "ali", last: "neaz" }, null, true, flase]; //aray but nested array
+
+
+//objects in jscript 
+
+// let nameA = {
+//     first: "Ali",
+//     last: "Neaz"
+// };
+// console.log( typeof(nameA));
+
+//nested objects in jscript 
+
+//undefined variable using let
+// let yourName;  //undefined variable
+// console.log(typeof (yourName));
+// let nothing = null; //datatype null 
+
+
+
+//string in jscript
+// let fruit = 'bAnAnA';
+// let moreFruit = 'banana \n apple';  //\n for new line 
+// // console.log(moreFruit);
+
+// let extraFruit = 'banana, apple , orange, komola, koal, jam, etc';
+// // console.log(extraFruit);
+// fruitlength = fruit.length;
+// console.log(fruitlength);
+// console.log(moreFruit.indexOf('an')); //finding index of some particular slice of word
+
+
+// console.log(fruit.slice(2, 5));
+// console.log(fruit.replace('ban', 'myName')); //will replace ban with myName
+// console.log(fruit.toUpperCase()); //will print to upper case
+
+// console.log(fruit.toLowerCase()); //wp all lowarCase 
+// console.log(fruit.charAt(1)); //everything starts with 0 
+// console.log(extraFruit.split(' '));
+
+
+
+
+
+
+
+
+//Array terms
+
+
+
